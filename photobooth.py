b797144c75d5a874917e5846a64efc7e1f4bdce0
sebcibaric <sebastijan.cibaric@gmail.com>
2021-09-02 01:10:26 +0200

    Doraden photobooth i argumenti
    Photobooth argumenti se sada predaju pomocu biblioteke argparse
    zbog jednostavnosti koristenja i citljivosti. Skripta sada
    okida 200 fotografija odjednom, 160 za treniranje modela i 40
    za validaciju.
    Take_photo takoder koristi argparse

--- photobooth.py
+++ photobooth.py
@@ -2,40 +2,36 @@ import sys
 import os 
 import cv2
 import time
+import argparse
 
-directions = ['UP', 'RIGHT', 'DOWN', 'LEFT']
+parser = argparse.ArgumentParser(description="Takes batch of photos")
+parser.add_argument('-n', '--name', type=str, nargs='+', help='Person\'s name, required argument', required=True)
 
-arg_len = len(sys.argv)
+try:
+    args = parser.parse_args()
+except:
+    parser.print_help()
+    sys.exit(0)
 
-if arg_len < 4:
-    print('[ERROR] Missing arguments! '
-          'Type in arguments for first name, last name purpose like: python3 photobooth.py john doe train/valid')
-    sys.exit()
-elif arg_len > 4:
-    print('[ERROR] Too many arguments! Type in arguments '
-          'for first name, last name and purpose like: python3 photobooth.py john doe train/valid')
-    sys.exit()
-elif arg_len == 4:
-    name = sys.argv[1] + '_' + sys.argv[2] 
-    purpose = sys.argv[3]
+name = '_'.join(args.name)
+purposes = ['train', 'valid']
 
-path = './dataset/{}/{}'.format(purpose, name)
+for purpose in purposes:
+    path = './dataset/{}/{}'.format(purpose, name)
+    if not os.path.exists(path):
+        print('[INFO] Path ' + path + ' does not exist. It will be created') 
+        os.mkdir(path)
 
-if not os.path.exists(path):
-    print('[INFO] Path ' + path + ' does not exist. It will be created') 
-    os.mkdir(path)
-
-
+purpose = purposes[0]
+lista = []
 cam = cv2.VideoCapture(0)
-direction_counter = 0
-for i in range(0, 40):
+for i in range(0, 200):
+    if i == 160:
+        purpose = purposes[1]
+    
+    path = './dataset/{}/{}'.format(purpose, name)
     time_to_int = int(time.time())
     timestamp = str(time_to_int + i)
-
-    if (i == 0 or i % 10 == 0) and direction_counter < 4:
-        print('Move your head: ' + directions[direction_counter])
-        direction_counter = direction_counter + 1
-        time.sleep(3)
 
     ret, frame = cam.read()
 
@@ -46,3 +42,5 @@ for i in range(0, 40):
     img = '{}/{}.jpg'.format(path, timestamp)
     cv2.imwrite(img, frame)
     print(img)
+
+cam.release()
--- take_photo.py
+++ take_photo.py
@@ -1,7 +1,15 @@
 import sys
 import cv2
+import argparse
 
-name = sys.argv[1]
+parser = argparse.ArgumentParser(description="Takes one photo")
+parser.add_argument('-n', '--name', type=str, nargs='+', help='Person\'s name, required argument', required=True)
+
+try:
+    args = parser.parse_args()
+except:
+    parser.print_help()
+    sys.exit(0)
 
 cam = cv2.VideoCapture(0)
 ret, frame = cam.read()
@@ -10,6 +18,7 @@ if not ret:
     print('[ERROR] Failed to take a picture')
     sys.exit()
 
+name = '_'.join(args.name)
 img = '{}.jpg'.format(name)
 cv2.imwrite(img, frame)
 print(img)
